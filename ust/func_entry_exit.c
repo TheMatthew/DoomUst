f1241776e95282c8e86d66a7d07b4399cc0149ea
Matthew Khouzam <matthew.khouzam@ericsson.com>
2012-06-30 00:16:21 -0400

    Added finstrument functions but no tracing.
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

---
+++ ust/func_entry_exit.c
@@ -0,0 +1,9 @@
+void __cyg_profile_func_enter(void *this_fn, void *call_site) __attribute__((no_instrument_function));
+
+void __cyg_profile_func_exit(void *this_fn, void *call_site) __attribute__((no_instrument_function));
+
+void __cyg_profile_func_enter(void *this_fn, void *call_site){
+}
+
+void __cyg_profile_func_exit(void *this_fn, void *call_site){
+}
